c0772043b7a298165425516f0fd598260a061b5d
songmm-b <songmm-b@glodon.com>
2019-01-03 18:11:12 +0800

    fix

--- springcloud-config-client/src/main/java/config/controller/ParamController.java
+++ springcloud-config-client/src/main/java/config/controller/ParamController.java
@@ -1,9 +1,7 @@
 package config.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +12,8 @@ public class ParamController {
 	@Value("${version}")
 	private String version;
 
-	@Autowired
-	private Environment environment ;
+	/*@Autowired
+	private Environment environment ;*/
 
 	/*@RequestMapping("/version")
 	public String getVersion(){
